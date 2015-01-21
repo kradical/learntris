73063a6ea01b9f02feacaad90275a7785145b24a
kradical <konrad_here@hotmail.com>
2015-01-20 23:52:59 -0800

    re-implemented data structure
    updated version v0.1.0
    almost caught up to where I was before, easier to deal with now

--- learntris.py
+++ learntris.py
@@ -1,38 +1,45 @@
-# learntris.py version 0.0.7
+# learntris.py version 0.1.0
 from sys import stdin
 import re
 from collections import defaultdict
 
+num_rows = 22
+num_cols = 10
+
+
 class GameState:
     def __init__(self):
-        self.row = []
-        for i in range(0, 22):
-            self.row.append(". . . . . . . . . .")
+        self.cell = []
+        for j in range(num_rows):
+            self.cell.append([])
+            for i in range(num_cols):
+                self.cell[j].append('.')
         self.score = 0
         self.lines_cleared = 0
         self.active_name = ''
+        self.top_left_active = [0, 0]
         self.active = []
         self.active_dictionary = defaultdict(list)
-        self.active_dictionary['cyan'] = ['. . . .', 'c c c c', '. . . .', '. . . .']
-        self.active_dictionary['yellow'] = ['y y', 'y y']
-        self.active_dictionary['red'] = ['r r .', '. r r', '. . .']
-        self.active_dictionary['green'] = ['. g g', 'g g .', '. . .']
-        self.active_dictionary['blue'] = ['b . .', 'b b b', '. . .']
-        self.active_dictionary['orange'] = ['. . o', 'o o o', '. . .']
-        self.active_dictionary['magenta'] = ['. m .', 'm m m', '. . .']
+        self.active_dictionary['cyan'] = [['.', '.', '.', '.'], ['c', 'c', 'c', 'c'], ['.', '.', '.', '.'], ['.', '.', '.', '.']]
+        self.active_dictionary['yellow'] = [['y', 'y'], ['y', 'y']]
+        self.active_dictionary['red'] = [['r', 'r', '.'], ['.', 'r', 'r'], ['.', '.', '.']]
+        self.active_dictionary['green'] = [['.', 'g', 'g'], ['g', 'g', '.'], ['.', '.', '.']]
+        self.active_dictionary['blue'] = [['b', '.', '.'], ['b', 'b', 'b'], ['.', '.', '.']]
+        self.active_dictionary['orange'] = [['.', '.', 'o'], ['o', 'o', 'o'], ['.', '.', '.']]
+        self.active_dictionary['magenta'] = [['.', 'm', '.'], ['m', 'm', 'm'], ['.', '.', '.']]
 
     def print_game_state(self):
-        for row in self.row:
-            print(row)
+        for row in self.cell:
+            print(' '.join(row))
 
     def update_game_state(self):
         stdin.read(1)
-        for ndx, row in enumerate(self.row):
-            self.row[ndx] = input()
+        for ndx, row in enumerate(self.cell):
+                self.cell[ndx] = input().split()
 
     def clear_game_state(self):
-        for ndx, row in enumerate(self.row):
-            self.row[ndx] = ". . . . . . . . . ."
+        for ndx, row in enumerate(self.cell):
+            self.cell[ndx] = ". . . . . . . . . .".split()
 
     def query_score(self):
         print(self.score)
@@ -41,114 +48,101 @@ class GameState:
         print(self.lines_cleared)
 
     def step_game(self):
-        for ndx, row in enumerate(self.row):
-            if '.' not in self.row[ndx]:
-                self.row[ndx] = ". . . . . . . . . ."
+        for ndx, row in enumerate(self.cell):
+            if '.' not in self.cell[ndx]:
+                self.cell[ndx] = ". . . . . . . . . .".split()
                 self.score += 100
                 self.lines_cleared += 1
 
     def initialize_active(self, tetranimo):
         self.active.clear()
+        for ndx1 in range(2):
+            for ndx2 in range(len(self.cell[0])):
+                self.cell[ndx1][ndx2] = '.'
         self.active_name = tetranimo
         for row in self.active_dictionary[tetranimo]:
             self.active.append(row)
-        for line in range(2):
-            if self.active_name == 'cyan':
-                self.row[line] = self.active[line].upper().join((". . . ", " . . ."))
-            elif self.active_name == 'yellow':
-                self.row[line] = self.active[line].upper().join((". . . . ", " . . . ."))
-            else:
-                self.row[line] = self.active[line].upper().join((". . . ", " . . . ."))
+        if self.active_name == 'yellow':
+            self.top_left_active = [0, 4]
+        else:
+            self.top_left_active = [0, 3]
+        self.set_active()
 
     def set_active(self):
-        if self.active_name == 'yellow':
-            for line in range(2):
-                self.row[line] = self.active[line].upper().join((". . . . ", " . . . ."))
-        elif self.active_name == 'cyan':
-            for line in range(4):
-                self.row[line] = self.active[line].upper().join((". . . ", " . . ."))
-        else:
-            for line in range(3):
-                self.row[line] = self.active[line].upper().join((". . . ", " . . . ."))
+        for ndx1 in range(len(self.active)):
+            for ndx2 in range(len(self.active)):
+                self.cell[ndx1][ndx2+self.top_left_active[1]] = self.active[ndx1][ndx2].upper()
 
     def check_active(self):
         for row in self.active:
-            print(row)
+            print(' '.join(row))
 
     def rotate_clockwise(self):
-        temp = []
-        for _ in self.active:
-            temp.append('')
-        for ndx2, col in enumerate(self.active):
-            for ndx1, row in enumerate(self.active):
-                temp[ndx2] += self.active[len(self.active)-1-ndx1][2*ndx2]+' '
-            temp[ndx2] = temp[ndx2][:-1]
-        self.active = temp
-        self.set_active()
+        self.active = list(zip(*self.active[::-1]))
 
     def rotate_anticlockwise(self):
-        temp = []
-        for _ in self.active:
-            temp.append('')
-        for ndx2, col in enumerate(self.active):
-            for ndx1, row in enumerate(self.active):
-                temp[ndx2] += self.active[ndx1][2*(len(self.active)-1)-2*ndx2]+' '
-            temp[ndx2] = temp[ndx2][:-1]
-        self.active = temp
-        self.set_active()
+        self.active = list(zip(*self.active))[::-1]
 
     @staticmethod
     def output_line():
         print()
 
     def nudge_left(self):
-        lines_that_need_shift = []
-        for ndx, line in enumerate(self.row):
-            match = re.search(r'[A-Z]', line)
-            if match:
-                lines_that_need_shift.append(ndx)
-                if match.start() == 0:
-                    return
-        for line in lines_that_need_shift:
-            self.row[line] = self.row[line][2:]+" ."
-
+        self.top_left_active[1] -= 1
+        print(self.top_left_active[1])
     def nudge_right(self):
         lines_that_need_shift = []
-        for ndx, line in enumerate(self.row):
+        for ndx, line in enumerate(self.cell):
             match = re.search(r'[A-Z]', line)
             if match:
                 lines_that_need_shift.append(ndx)
                 if line[-1:].isupper():
                     return
         for line in lines_that_need_shift:
-            self.row[line] = ". "+self.row[line][:-2]
+            self.cell[line] = ". "+self.cell[line][:-2]
+
+    def detect_collision(self, ndx):
+        if ndx == len(self.cell)-1:
+            return True
+        matches_upper = re.finditer(r'[A-Z]', self.cell[ndx])
+        matches_lower = re.finditer(r'[a-z]', self.cell[ndx+1])
+        for matchU in matches_upper:
+            for matchL in matches_lower:
+                if matchU.start() == matchL.start():
+                    return True
+        return False
 
     def nudge_down(self):
-        for ndx, line in enumerate(self.row):
-            ndx2 = len(self.row) - ndx - 1
-            match = re.search(r'[A-Z]', self.row[ndx2])
+        for ndx, line in enumerate(self.cell):
+            ndx2 = len(self.cell) - ndx - 1
+            match = re.search(r'[A-Z]', self.cell[ndx2])
             if match:
-                if ndx == 0:
-                    return
+                if self.detect_collision(ndx2):
+                    return False
                 else:
-                    self.row[ndx2], self.row[ndx2+1] = self.row[ndx2+1], self.row[ndx2]
+                    self.swap_elements(ndx2)
+        return True
+
+    def swap_elements(self, ndx2):
+        upper_row = ndx2
+        lower_row = ndx2+1
+        matches_upper = re.finditer(r'[A-Z]', self.cell[upper_row])
+        matches_lower = re.finditer(r'[A-Z]', self.cell[lower_row])
+        for matchU in matches_upper:
+            if self.cell[lower_row][matchU.start()].islower():
+                print(self.cell[lower_row][matchU.start()])
+            else:
+                pass
+        self.cell[ndx2], self.cell[ndx2+1] = self.cell[ndx2+1], self.cell[ndx2]
 
     def place_tiles(self):
-        for ndx, row in enumerate(self.row):
-            self.row[ndx] = row.lower()
+        for ndx, row in enumerate(self.cell):
+            self.cell[ndx] = row.lower()
 
     def drop_down(self):
-        while True:
-            for ndx, line in enumerate(self.row):
-                ndx2 = len(self.row) - ndx - 1
-                match = re.search(r'[A-Z]', self.row[ndx2])
-                if match:
-                    if ndx == 0:
-                        self.place_tiles()
-                        return
-                    else:
-                        self.row[ndx2], self.row[ndx2+1] = self.row[ndx2+1], self.row[ndx2]
-
+        while self.nudge_down():
+            pass
+        self.place_tiles()
 
 
 x = GameState()
